c57d2692cfef814deff167405fcc4e7d169b4b9d
littleBIGloverX <KVJgaming739@gmail.com>
2017-06-22 13:51:15 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -14,7 +14,7 @@ var l_serverImage = "logo.png";
 
 /*Custom server name configuration
 Leave it blank for automatic server name*/
-var l_serverName = "";
+var l_serverName = "ChaotiK DRP";
 
 /*Use a video for the background?
 Otherwise it will just use images*/
